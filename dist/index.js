adec14ace6ae5f1a25e1a9d34198ec9af9c99ae7
basilmaben <basilmaben.bm@gmail.com>
2021-09-09 12:45:35 +0530

    mikro-orm setup complete

--- dist/index.js
+++ dist/index.js
@@ -8,8 +8,9 @@ const Post_1 = require("./entities/Post");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
 const main = async () => {
     const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
-    const post = orm.em.create(Post_1.Post, { title: 'my first post' });
-    await orm.em.persistAndFlush(post);
+    await orm.getMigrator().up();
+    const posts = await orm.em.find(Post_1.Post, {});
+    console.log(posts);
 };
 main().catch((err) => {
     console.log(err);
