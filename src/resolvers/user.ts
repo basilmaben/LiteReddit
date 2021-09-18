e4a9d7655dff77699b31ffdf2ecc19965c5134dd
basilmaben <basilmaben.bm@gmail.com>
2021-09-18 11:37:50 +0530

    argon2 mutation test

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,12 +1,13 @@
 import { User } from '../entities/User';
 import { MyContext } from '../types';
 import { Resolver, Mutation, Arg, InputType, Field, Ctx } from "type-graphql";
-import  argon2 from "argon2";
+import argon2 from 'argon2';
+
 
 
 
 @InputType()
-export class UsernamePasswordInput{
+export class UsernamePasswordInput {
     @Field()
     username: string
     @Field()
@@ -23,6 +24,6 @@ export class UserResolver{
     const hashedPassword = await argon2.hash(options.password)
     const user = em.create(User, { username: options.username,  password: hashedPassword})
     await em.persistAndFlush(user)
-        return user
+        return user;
     }
 }
